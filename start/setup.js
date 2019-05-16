f3b525e022383998a11016bbc0118648692729c8
= <craig@craigshoemaker.net>
2019-05-16 11:00:15 -0400

    Add more seed data records

--- start/setup.js
+++ start/setup.js
@@ -2,13 +2,6 @@ const client = require('./db.js');
 
 const databaseDefinition = { id: "stocksdb" };
 const collectionDefinition = { id: "stocks" };
-const stock = {
-    "id": "e0eb6e85-176d-4ce6-89ae-1f699aaa0bab",
-    "symbol": "ABC",
-    "price": "100.00",
-    "change": "0.00",
-    "changeDirection": "+"
-};
 
 const setupAndSeedDatabase = async ()  => {
 
@@ -18,10 +11,33 @@ const setupAndSeedDatabase = async ()  => {
   const { container } = await db.containers.create(collectionDefinition);
   console.log('Collection created.');
 
-  await container.items.create(stock);
+  await container.items.create({
+    "id": "e0eb6e85-176d-4ce6-89ae-1f699aaa0bab",
+    "symbol": "ABC",
+    "price": "100.00",
+    "change": "1.00",
+    "changeDirection": "+"
+  });
+
+  await container.items.create({
+    "id": "ebe2e863-bf84-439a-89f8-39975e7d6766",
+    "symbol": "DEF",
+    "price": "45.89",
+    "change": "1.25",
+    "changeDirection": "-"
+  });
+
+  await container.items.create({
+    "id": "ebe2e863-bf84-439a-89f8-39975e7d6766",
+    "symbol": "GHI",
+    "price": "156.21",
+    "change": "6.81",
+    "changeDirection": "+"
+  });
+
   console.log('Seed data added.');
 };
 
 setupAndSeedDatabase().catch(err => {
-  console.error(err);
+  console.error('Error setting up database:', err);
 });
